a87ddf31b25f5a72841754ab39311f681586947c
28vicky <28vicky@users.noreply.github.com>
2014-06-11 01:35:43 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,6 +17,7 @@ add_lunch_combo carbon_i9300-userdebug
 add_lunch_combo carbon_i9305-userdebug
 add_lunch_combo carbon_i9500-userdebug
 add_lunch_combo carbon_infuse4g-userdebug
+add_lunch_combo carbon_janice-userdebug
 add_lunch_combo carbon_jflte-userdebug
 add_lunch_combo carbon_l900-userdebug
 add_lunch_combo carbon_ls980-userdebug
